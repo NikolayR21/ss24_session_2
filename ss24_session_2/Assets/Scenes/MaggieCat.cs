c9498ff2cac63e7edc1f99c60935e3617617ddaf
MaggieMou <eaamalis@students.eaaa.dk>
2024-03-21 12:13:27 +0100

    My cat
    It's my cat

---
+++ ss24_session_2/Assets/Scenes/MaggieCat.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MaggieCat : MonoBehaviour
+{
+    // Public properties
+    public string name;
+    public int age;
+
+    // Private properties
+    [SerializeField] private string _color;
+    [SerializeField] private string _breed;
+}
